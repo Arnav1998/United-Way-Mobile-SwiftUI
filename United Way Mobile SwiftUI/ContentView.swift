1aff2b277a27ed3bd2ee36a8f539e78898e1cb71
Arnav Singhania <arnavsinghania98@gmail.com>
2019-11-10 18:38:53 -0800

    Initial Commit

---
+++ United Way Mobile SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  United Way Mobile SwiftUI
+//
+//  Created by ARNAV SINGHANIA on 11/10/19.
+//  Copyright © 2019 ARNAV SINGHANIA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
